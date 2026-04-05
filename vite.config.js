ffa11f49390cd379b44dc61a58a589361562e5dc
neslihankaradenizz <neslihan.krdnz53@gmail.com>
2026-04-05 18:18:52 +0300

    Fusebox interface+pwa

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,34 @@ export default defineConfig({
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
+      manifest: {
+        name: 'AIO-FuseBox Vision',
+        short_name: 'FuseBox',
+        description: 'AIO-Fusebox Vision',
+        theme_color: '#0d0d0f',
+        background_color: '#0d0d0f',
+        display: 'standalone', 
+        orientation: 'portrait',
+        icons: [
+          {
+            src: '/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          }
+        ]
+      },
       workbox: {
         globPatterns: ['**/*.{js,css,html,json}'],
         maximumFileSizeToCacheInBytes: 150 * 1024 * 1024,
         runtimeCaching: [
           {
-            urlPattern: /^https:\/\/fusebox-assets\..*\.workers\.dev\/.*/i,
+            urlPattern: /^https:\/\/aoi-fusebox1\.neslihan-krdnz53\.workers\.dev\/.*/i,
             handler: 'CacheFirst',
             options: {
               cacheName: 'r2-heavy-assets',
               expiration: {
                 maxEntries: 15,
-                maxAgeSeconds: 60 * 60 * 24 * 90, // 90 gün
+                maxAgeSeconds: 60 * 60 * 24 * 90,
               },
             },
           },

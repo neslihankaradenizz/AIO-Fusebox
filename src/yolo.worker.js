a03fb3de151a60d6de4d35768258b58168a1aa0d
neslihankaradenizz <neslihan.krdnz53@gmail.com>
2026-04-14 08:23:26 +0300

    yolo.worker update-5

--- src/yolo.worker.js
+++ src/yolo.worker.js
@@ -4,29 +4,17 @@ import { loadModel, runInference, preprocessCanvas, postprocess } from './yolo.j
 const ORT_BASE = 'https://aoi-fusebox1.neslihan-krdnz53.workers.dev/';
 
 async function loadOrt() {
-  const ortModule = await import(ORT_BASE + 'ort-wasm-simd-threaded.mjs');
   
-  // Emscripten factory'ye locateFile ile WASM path'ini söyle
-  const ort = await ortModule.default({
-    locateFile: (filename) => {
-      console.log('[Worker] locateFile:', filename);
-      return ORT_BASE + filename;
-    }
-  });
+  const ortModule = await import(ORT_BASE + 'ort-wasm-simd-threaded.jsep.mjs');
+  
+  console.log('[Worker] jsep keys:', Object.keys(ortModule));
 
-  console.log('[Worker] ort type:', typeof ort);
-  console.log('[Worker] ort keys:', Object.keys(ort ?? {}));
+  
+  self.ort = ortModule;
 
-  if (!ort) throw new Error('ORT factory undefined döndü');
-
-  self.ort = ort;
-
-  // env sadece gerçek ORT objesinde olur
-  if (self.ort.env) {
-    self.ort.env.wasm.wasmPaths = ORT_BASE;
-    self.ort.env.wasm.numThreads = 1;
-    self.ort.env.wasm.proxy = false;
-  }
+  self.ort.env.wasm.wasmPaths = ORT_BASE;
+  self.ort.env.wasm.numThreads = 1;
+  self.ort.env.wasm.proxy = false;
 
   console.log('[Worker] ORT hazır, InferenceSession:', typeof self.ort.InferenceSession);
 }

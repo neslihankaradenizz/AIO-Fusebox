8d31a6aa9d07b9a4cc1e4902a7115870a3c4eac1
neslihankaradenizz <neslihan.krdnz53@gmail.com>
2026-04-21 08:21:15 +0300

    app.js update

--- src/app.js
+++ src/app.js
@@ -171,16 +171,16 @@ function workerInfer(imageBitmap, width, height) {
       }
     };
     worker.addEventListener('message', onMsg);
-   //**  
-   // worker.postMessage(
-  //   { type: 'infer', 
-   //     payload: { 
-   //       bitmap,
-   //       width: video.videoWidth,
-   //       height: video.videoHeight,
-   //       roi: getRoiRect(video)
-   //     } },
-   //   [bitmap]); 
+    
+    worker.postMessage(
+     { type: 'infer', 
+        payload: { 
+          bitmap:imageBitmap,
+          width: video.videoWidth,
+          height: video.videoHeight,
+          roi: getRoiRect(video)
+        } },
+   [imageBitmap]); 
   
   });
 }
